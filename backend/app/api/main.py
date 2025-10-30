7a88fd8475039dc1efe77f05067038d91cad64f4
Lukasz Filo <lukif02@gmail.com>
2025-10-30 22:23:34 +0100

    dependencies added, routers added

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 
-from .routers import (health_router, doctor_router, nurse_router)
+from .routers import (health_router, doctor_router, nurse_router,
+                      receptionist_router, register_router, reservation_router)
 
 api_router = APIRouter()
 
 api_router.include_router(health_router.router)
 api_router.include_router(doctor_router.router)
 api_router.include_router(nurse_router.router)
+api_router.include_router(receptionist_router.router)
+api_router.include_router(register_router.router)
+api_router.include_router(reservation_router.router)
