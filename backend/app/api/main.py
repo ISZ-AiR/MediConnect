ddad0f355e66c9790a2e21799daa6b14b81b8de6
konradflis1 <k.flis101@gmail.com>
2025-11-02 13:27:41 +0100

    Routers added to main; poetry update

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .routers import (health_router, doctor_router, nurse_router,
-                      referral_router, visit_router, prescription_router)
+                      referral_router, visit_router, prescription_router,
+                      login_router, receptionist_router, register_router,
+                      reservation_router)
 
 api_router = APIRouter()
 
@@ -14,3 +16,11 @@ api_router.include_router(referral_router.router)
 api_router.include_router(visit_router.router)
 
 api_router.include_router(prescription_router.router)
+
+api_router.include_router(login_router.router)
+
+api_router.include_router(receptionist_router.router)
+
+api_router.include_router(register_router.router)
+
+api_router.include_router(reservation_router.router)
